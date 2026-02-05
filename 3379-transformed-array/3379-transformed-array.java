5c5d6c4b3c7f449656d67dd2859986ebea7b7611
Hamsiny C <hamsinyparimala2007@gmail.com>
2026-02-05 22:32:42 +0530

    Time: 1 ms (98.72%), Space: 47.1 MB (13.78%) - LeetHub

---
+++ 3379-transformed-array/3379-transformed-array.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] constructTransformedArray(int[] nums) {
+        int n=nums.length;
+        int[] result=new int[n];
+        for(int i=0;i<n;i++){
+            if(nums[i]==0){
+                result[i]=0;
+            }
+            else if(nums[i]>0){
+                int newIndex=(i+nums[i])%n;
+                result[i]=nums[newIndex];
+            }
+            else{
+               int newIndex=(i+nums[i])%n;
+               if(newIndex<0){
+                newIndex=newIndex+n;
+               }
+               result[i]=nums[newIndex];
+            }
+        }
+        return result;
+    }
+}
