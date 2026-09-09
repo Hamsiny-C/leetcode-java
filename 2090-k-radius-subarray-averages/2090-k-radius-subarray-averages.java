d23445a1098b9a95f39462d4b74ac0d6703b6612
Hamsiny C <hamsinyparimala2007@gmail.com>
2026-09-09 10:36:07 +0530

    Time: 4 ms (99.11%), Space: 175.7 MB (20.61%) - LeetHub

--- 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.java
+++ 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] getAverages(int[] nums, int k) {
-        int n = nums.length;
+        /*int n = nums.length;
         int[] ans = new int[n];
         long sum = 0;
         int size = 2 * k + 1;
@@ -17,6 +17,27 @@ class Solution {
             sum += nums[i];
             ans[i - k] = (int)(sum / size);
         }
-        return ans;
+        return ans;*/
+
+        int[] res=new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            res[i]=-1;
+        }
+        int wsize=2*k+1;
+        if(wsize>nums.length){
+            return res;
+        }
+        long sum=0;
+        for(int i=0;i<wsize;i++){
+            sum+=nums[i];
+        }
+        int avg=(int)(sum/wsize);
+        res[k]=avg;
+        
+        for(int i=wsize;i<nums.length;i++){
+           sum=sum+nums[i]-nums[i-wsize];
+          res[i-k]=(int)(sum/wsize);
+        }
+        return res;
     }
 }
